27a4b7bd752d39c8de07d980fa6bb1a99725174a
Simone <marzolla.istruzione@gmail.com>
2021-04-16 16:47:16 +0200

    Esercizio concluso

---
+++ script.js
@@ -0,0 +1,21 @@
+/*  Attraverso una chiamata ajax all'API di boolean
+    https://flynn.boolean.careers/exercises/api/array/music
+    avremo a disposizione una decina di dischi musicali.
+    Utilizzando vue, stampiamo a schermo una card per ogni album.*/
+
+function init() {
+  new Vue({
+    el: '#app',
+    data: {
+      disc: []
+    },
+    mounted() {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then(data =>{
+        this.disc = data.data.response;
+        console.log(data.data.response);
+      })
+    }
+  })
+}
+document.addEventListener("DOMContentLoaded", init);
